83ac471e38aab3f170697ac996328127c1e5f2ae
Isaac Dimitrovsky <ikedim@gmail.com>
2021-06-03 09:45:50 -0400

    Filling in utils.

--- secscan/_nbdev.py
+++ secscan/_nbdev.py
@@ -5,7 +5,16 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"stockDataRoot": "00_utils.ipynb",
          "requestUrl": "00_utils.ipynb",
          "downloadSecUrl": "00_utils.ipynb",
-         "secUrlPref": "00_utils.ipynb"}
+         "secUrlPref": "00_utils.ipynb",
+         "pageUnavailableRE": "00_utils.ipynb",
+         "openFp": "00_utils.ipynb",
+         "pickSave": "00_utils.ipynb",
+         "pickLoad": "00_utils.ipynb",
+         "pickLoadIfPath": "00_utils.ipynb",
+         "savePklToDir": "00_utils.ipynb",
+         "loadPklFromDir": "00_utils.ipynb",
+         "saveSplitPklToDir": "00_utils.ipynb",
+         "loadSplitPklFromDir": "00_utils.ipynb"}
 
 modules = ["utils.py"]
 
